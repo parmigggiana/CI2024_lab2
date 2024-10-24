be66d78485b26baa80d5a383df73c36039c0a37e
Francesco Basile <59255877+parmigggiana@users.noreply.github.com>
2024-10-24 23:49:33 +0200

    finish the outline of the script

--- main.py
+++ main.py
@@ -4,21 +4,24 @@ Using Tournament Selection, Crossover, and Mutation
 """
 
 import itertools
-import multiprocessing
+import concurrent.futures
 import pandas as pd
 import numpy as np
 import geopy.distance
 from typing import Self
 from icecream import ic
+import matplotlib.pyplot as plt
 
-FILENAME = "vanuatu.csv"
+FILENAME = "italy.csv"
 POPULATION_SIZE = 50
-TOURNAMENTS = 5
+TOURNAMENTS = 2
 REPRODUCTIVE_RATE = 2
 PARENTS = 10
-CHAMPIONS = 1
-MUTATION_RATE = 2
-MAX_AGE = 2
+CHAMPIONS_PER_TOURNAMENT = 0
+MUTATION_RATE = 0.4
+MAX_AGE = 1
+MIN_ITERS = 400
+WINDOW_SIZE = 2
 
 rng = np.random.Generator(np.random.PCG64(0xDEADBEEF))
 
@@ -35,7 +38,7 @@ class Geneset:
         for i in range(self.len):
             self.cost += dist_mat[self.genes[i], self.genes[i - 1]]
 
-    def mutate(self, prob: float) -> Self:
+    def mutate(self, prob: float) -> Self:  # TODO make it better :)
         """Return a new Geneset mutating each gene with probability `prob`
             Mutation is done by swapping two adjacent genes
             Always keep the first gene fixed
@@ -64,22 +67,27 @@ class Geneset:
         return self.cost <= other.cost
 
 
-def xover(population):
+def xover(population):  # TODO proper crossover
     # Randomly crossover pairs of parents to create POPULATION_SIZE children
     # Always keep the first gene fixed
-    children = []
-    for _ in range(POPULATION_SIZE * REPRODUCTIVE_RATE):
+    children = np.empty(POPULATION_SIZE * REPRODUCTIVE_RATE, dtype=Geneset)
+    for i in range(POPULATION_SIZE * REPRODUCTIVE_RATE):
         parent1, parent2 = rng.choice(population, 2, replace=False)
         child = Geneset(parent1.genes.copy())
         start, end = rng.choice(range(1, child.len), 2, replace=False)
         child.genes[start:end] = parent2.genes[start:end]
-        children.append(child)
+        child.genes = parent1.genes
+        children[i] = child
 
     return children
 
 
-def select_parents(population):
-    return population[:PARENTS]
+def select_parents(population: np.ndarray):
+    population.sort()
+    selected = population[: PARENTS // TOURNAMENTS]
+    for g in selected[CHAMPIONS_PER_TOURNAMENT:]:
+        g.age += 1
+    return selected
 
 
 def select(population):
@@ -115,35 +123,78 @@ def main(filename):
     # genes = np.insert(np.random.permutation(df.shape[0] - 1) + 1, 0, 0)
     # geneset = np.insert(np.random.permutation(np.arange(1, df.shape[0])), 0, 0)
 
-    population = [
-        Geneset(genes)
-        for genes in [
-            np.insert(rng.permutation(np.arange(1, df.shape[0])), 0, 0)
-            for _ in range(POPULATION_SIZE)
+    population = np.array(
+        [
+            Geneset(genes)
+            for genes in [
+                np.insert(rng.permutation(np.arange(1, df.shape[0])), 0, 0)
+                for _ in range(POPULATION_SIZE)
+            ]
         ]
-    ]
-    [geneset.compute_cost(distance_matrix) for geneset in population]
+    )
+    compute_costs(population, distance_matrix)
 
-    with multiprocessing.Pool() as pool:
+    best_geneset = population[0].genes
+    best_cost = population[0].cost
+    i = 0
+    history = []
+
+    with concurrent.futures.ThreadPoolExecutor(TOURNAMENTS) as pool:
         while True:
-            # ic(population)
-            batches = np.array_split(population, PARENTS)
+            i += 1
+            batches = np.array_split(population, TOURNAMENTS)
 
-            parents = pool.map(select_parents, batches)
-            # parents = select_parents(population)
-            # ic(parents)
-            children = pool.map(xover, parents)
-            # ic(children)
-            mutated_children = pool.map(mutate, children)
-
-            # ic(mutated_children)
-            pool.starmap(
-                compute_costs,
-                [(children, distance_matrix) for children in mutated_children],
+            futures = {pool.submit(select_parents, batch) for batch in batches}
+            parents = np.concatenate(
+                [f.result() for f in concurrent.futures.as_completed(futures)],
             )
-            ic(mutated_children)
-            population = select(population + mutated_children)
-            break
+            children = xover(parents)
+            futures = {pool.submit(child.mutate, MUTATION_RATE) for child in children}
+            mutated_children = np.array(
+                [f.result() for f in concurrent.futures.as_completed(futures)]
+            )
+            concurrent.futures.wait(
+                {
+                    pool.submit(geneset.compute_cost, distance_matrix)
+                    for geneset in mutated_children
+                }
+            )
+
+            population = np.concatenate([population, mutated_children])
+            population = select(population)
+
+            history.append([g.cost for g in population])
+            if population[0].cost < best_cost:
+                best_cost = population[0].cost
+                best_geneset = population[0].genes
+
+            improvement_rate = (
+                (history[-1][0] - history[-i // WINDOW_SIZE][0]) / (i // WINDOW_SIZE)
+                if i > MIN_ITERS
+                else np.inf
+            )
+            if improvement_rate < 0.01:
+                break
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for j, h in enumerate(history):
+        ax.scatter(
+            [j] * len(h),
+            h,
+            color="blue",
+            alpha=1 / POPULATION_SIZE,
+            marker=".",
+        )
+    ax.plot([h[0] for h in history], color="red")
+    ax.set_title(f"iterations: {i} - best cost: {best_cost:.2f}")
+    print(f"Iterations: {i}")
+    print(f"Path cost: {best_cost:.2f}")
+    print(f"visited cities:\n{df.iloc[best_geneset].name}")
+    print(
+        f"Wolfram-coded path: {{{', '.join(f'{best_geneset + 1}'[1:-1].split())}, 1}}"
+    )
+    plt.show()
 
 
 if __name__ == "__main__":
